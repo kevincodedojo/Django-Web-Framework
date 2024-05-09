075f2376a471f963a293fa383325c8a3e9a5561e
kevincodedojo <ashen7@mail.ccsf.edu>
2024-05-09 15:01:08 -0700

    views and url configuration

--- Week 1/builDjango/chefsTable/chefsTable/urls.py
+++ Week 1/builDjango/chefsTable/chefsTable/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
 
-from demoapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='index'),
+    # path('', views.index, name='index'),
+    path('', include('demoapp.urls')),
 
 ]
--- Week 1/builDjango/chefsTable/demoapp/views.py
+++ Week 1/builDjango/chefsTable/demoapp/views.py
@@ -2,5 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+
 def index(request):
     return HttpResponse("Hello, world. This is  the index view of the DemoApp")
+
+def home(request):
+    return HttpResponse("<h1> Welcome to the Little Lemon! </h1>")
+
+    
