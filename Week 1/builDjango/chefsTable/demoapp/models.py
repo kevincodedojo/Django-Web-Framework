c2816db953a9709577b40f95115b0eb991b82529
“kevin” <ashen7@mail.ccsf.edu>
2024-05-18 19:38:52 -0700

    update

--- Week 1/builDjango/chefsTable/demoapp/models.py
+++ Week 1/builDjango/chefsTable/demoapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Drinks (models.Model):
+    drink_name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    category = models.ForeignKey('DrinksCategory', on_delete=models.PROTECT, default=None)
+
+class DrinksCategory(models.Model):
+    category_name = models.CharField(max_length=100)
+    
